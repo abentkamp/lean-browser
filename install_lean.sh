5aad452da850235afd4fb20bdab985336615fe02
Alexander Bentkamp <bentkamp@gmail.com>
2022-08-05 21:15:22 +0200

    more

--- install_lean.sh
+++ install_lean.sh
@@ -1,3 +1,3 @@
 curl https://raw.githubusercontent.com/leanprover/elan/master/elan-init.sh -sSf | sh -s -- -y --default-toolchain leanprover/lean4:nightly
 ln -s ~/.elan/bin/lean lean
-lean
+~/.elan/bin/lean
