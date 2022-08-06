b055dd75c6e64e94b4024ed155c0732f0c0e1b7b
Alexander Bentkamp <bentkamp@gmail.com>
2022-08-06 18:30:24 +0200

    handle multiple clients

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const express = require("express");
 const app = express()
 const path = require("path")
 const fs = require("fs");
+const { spawn } = require('child_process');
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const server = app
   .use(express.static('./build'))
@@ -13,22 +14,93 @@ const server = app
 
 const wss = new WebSocket.Server({ server })
 
-function send(data) {
-    const str = JSON.stringify(data) + "\r\n";
-    const byteLength = Buffer.byteLength(str, "utf-8");
+let leanCmd = "./lean";
+if (!fs.existsSync(leanCmd)) {
+    leanCmd = "lean"
+}
 
-    lean.stdin.cork();
-    lean.stdin.setEncoding('ascii');
-    lean.stdin.write(`Content-Length: ${byteLength}\r\n\r\n`);
-    lean.stdin.setEncoding('utf-8');
-    lean.stdin.write(str);
-    lean.stdin.uncork();
+class ClientConnection {
+
+    header = ""
+    content = ""
+    contentLength = null
+    headerMode = true
+
+    re = /Content-Length: (\d+)\r\n/i;
+
+    constructor(ws){
+        console.log("Socket opened.")
+        this.ws = ws    
+        this.ws.on("message", (msg) => {
+            this.send(JSON.parse(msg.toString("utf8")));
+        })
+
+        this.ws.on("close", () => {
+            this.lean.kill();
+            console.log("Socket closed.")
+        })
+
+        this.lean = spawn(leanCmd, ['--server']);
+
+        this.lean.stdout.on('readable', () => {
+            this.read();
+        });
+
+        this.lean.stderr.on('data', (data) => {
+            console.error(`stderr: ${data}`);
+        });
+    }
+
+    read () {
+        if (this.headerMode) {
+            let chr;
+            while (chr = this.lean.stdout.read(1)) {
+                chr = chr.toString("ascii")
+                this.header += chr
+                if (this.header.endsWith("\r\n\r\n")) {
+                    var found = this.header.match(this.re);
+                    this.contentLength = parseInt(found[1]);
+                    // console.log(`HEADER: ${header}`);
+                    // console.log(`LENGTH: ${contentLength}`);
+                    this.content = ""
+                    this.headerMode = false;
+                    this.read();
+                }
+            }
+        } else {
+            let str;
+            while (str = this.lean.stdout.read(Math.min(this.lean.stdout.readableLength, this.contentLength))) {
+                this.contentLength -= str.length
+                str = str.toString("utf8")
+                this.content += str;
+                if (this.contentLength <= 0) {
+                    
+                    if (this.ws.readyState === WebSocket.OPEN) {     // check if client is ready
+                        this.ws.send(this.content);
+                    }
+                    this.headerMode = true;
+                    this.header = "";
+                    this.read();
+                }
+            }
+        }
+    }
+
+    send(data) {
+        const str = JSON.stringify(data) + "\r\n";
+        const byteLength = Buffer.byteLength(str, "utf-8");
+    
+        this.lean.stdin.cork();
+        this.lean.stdin.setEncoding('ascii');
+        this.lean.stdin.write(`Content-Length: ${byteLength}\r\n\r\n`);
+        this.lean.stdin.setEncoding('utf-8');
+        this.lean.stdin.write(str);
+        this.lean.stdin.uncork();
+    }
 }
 
 wss.on("connection", function(ws) {    // what should a websocket do on connection
-    ws.on("message", function(msg) {        // what to do on message event
-        send(JSON.parse(msg.toString("utf8")));
-    })
+    new ClientConnection(ws)
 })
 
 // server.on('upgrade', async function upgrade(request, socket, head) {
@@ -37,61 +109,6 @@ wss.on("connection", function(ws) {    // what should a websocket do on connecti
 //     });
 // });
 
-const { spawn } = require('child_process');
 
-let leanCmd = "./lean";
-if (!fs.existsSync(leanCmd)) {
-    leanCmd = "lean"
-}
 
-const lean = spawn(leanCmd, ['--server']);
 
-var header = ""
-var content = ""
-var contentLength = null
-var headerMode = true
-
-var re = /Content-Length: (\d+)\r\n/i;
-
-function read () {
-    if (headerMode) {
-        while (chr = lean.stdout.read(1)) {
-            chr = chr.toString("ascii")
-            header += chr
-            if (header.endsWith("\r\n\r\n")) {
-                var found = header.match(re);
-                contentLength = parseInt(found[1]);
-                // console.log(`HEADER: ${header}`);
-                // console.log(`LENGTH: ${contentLength}`);
-                content = ""
-                headerMode = false;
-                read();
-            }
-        }
-    } else {
-        while (str = lean.stdout.read(Math.min(lean.stdout.readableLength, contentLength))) {
-            contentLength -= str.length
-            str = str.toString("utf8")
-            content += str;
-            if (contentLength <= 0) {
-                
-                wss.clients.forEach(function each(client) {
-                    if (client.readyState === WebSocket.OPEN) {     // check if client is ready
-                        client.send(content);
-                    }
-                })
-                headerMode = true;
-                header = "";
-                read();
-            }
-        }
-    }
-}
-
-lean.stdout.on('readable', () => {
-    read();
-});
-
-lean.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
-});
